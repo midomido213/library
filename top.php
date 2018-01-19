805992e2abb7402df41703fb8de3784e99f18bed
midomido213 <midopoppo213@gmail.com>
2018-01-19 13:49:32 +0900

    レスポンシブにした

--- top.php
+++ top.php
@@ -5,9 +5,14 @@
 <head>
   <title>トップメニュー</title>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
   <!-- <link rel="stylesheet" href="style.css"> -->
   <link rel="stylesheet" href="bootflat.github.io-master/css/site.min.css">
   <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,800,700,400italic,600italic,700italic,800italic,300italic" rel="stylesheet" type="text/css">
+
+  <script type="text/javascript" src="bootflat.github.io-master/js/site.min.js"></script>
 </head>
 
 <body>
