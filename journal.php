4c1ca96fe19c4f03e94a705bfac31a314b058f5c
ShutaKimura <g031n051@s.iwate-pu.ac.jp>
2018-01-24 11:50:37 +0900

    電子ジャーナルの更新

--- journal.php
+++ journal.php
@@ -18,13 +18,11 @@ $account = $_SESSION['account'];
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1">
 
-  <!-- <link rel="stylesheet" href="style.css"> -->
+  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="bootflat.github.io-master/css/site.min.css">
   <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,800,700,400italic,600italic,700italic,800italic,300italic" rel="stylesheet" type="text/css">
 
   <script type="text/javascript" src="bootflat.github.io-master/js/site.min.js"></script>
-
-  <title>電子ジャーナル</title>
 </head>
 <body class="main">
   <!-- header -->
@@ -35,6 +33,21 @@ $account = $_SESSION['account'];
     </div>
   </div>
 
-    <h3>電子ジャーナル</h3>
+  <!-- documents -->
+  <div class="container documents">
+    <h3>電子ジャーナル</h3><br>
 
-    <h5>電子ジャーナルの閲覧は学内限定であるため，メディアセンターと連携すれば見れるようになります．</h5>
+    <div>電子ジャーナルの閲覧は学内限定であるため，メディアセンターと連携すれば見れるようになります．</div><br>
+
+    <div>
+      <button onclick="history.back()" class="btn btn-normal">戻る</button>
+    </div><br>
+
+  </div>
+</div>
+<!-- footer -->
+<div class="site-footer">
+  <p>© 2017 岩手県立大学 ソフトウェア情報学部 佐々木研 D-9-e</p>
+</div>
+</body>
+</html>
